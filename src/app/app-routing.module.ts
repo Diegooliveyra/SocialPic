2fab7fa24bdd4af0908bdddcd8c3619649a35458
diegooliveyra <diegooliveirabryto@gmail.com>
2021-09-15 23:00:43 -0300

    :sparkles: criado a rota para o componente not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NgModule } from '@angular/core';
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'photo/add',
     component: PhotoFormComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
 
